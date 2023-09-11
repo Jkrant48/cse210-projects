bee1a3c7f310f8d49232b835c9b8989e1f1f630b
Jonathan Koranteng <kor22002@byui.edu>
2023-09-11 19:28:57 +0000

    finished prep1 and prep2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Runtime.ConstrainedExecution;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {   
+        // prompt user for their first name and last name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Ask user for their grade percentage
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        // letter grade
+        string letter;
+
+        if (grade >= 90)
+            letter = "A";
+
+        else if (grade >= 80)
+            letter = "B";
+
+        else if (grade >= 70)
+            letter = "C";
+
+        else if (grade >= 60)
+            letter = "D";
+            
+        else
+            letter = "F";
+
+        Console.WriteLine(letter);
+        
+        // a separate if and else statement for if the student passed
+        if (grade >= 70)
+            Console.WriteLine("Congratulations!! You passed.");
+        else 
+            Console.WriteLine("Don't give up! This is just a minor set back.");
+        
+    
+
     }
 }
