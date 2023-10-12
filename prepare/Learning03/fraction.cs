35d084147dcc763f2b51ee03fc4e735a79e88510
Jonathan Koranteng <kor22002@byui.edu>
2023-10-12 18:05:18 +0000

    finished learning activity

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,60 @@
+// fraction class (encapsulation practice)
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+
+        ;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    /*public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int bottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }*/
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        double fraction = (double)_top / _bottom;
+        return fraction;
+    }
+}
+
